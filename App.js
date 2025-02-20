fd9e7d5d29141f630ca20d2b0af9d573a57d463d
GVSrid <152718647+GVSrid@users.noreply.github.com>
2025-02-20 17:51:13 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
-
 //import './App.css';
-import Sidebar from './Sidebar';
-import Header from './Header';
-import Content from './Content'
+import Sidebar from './Components/Sidebar';
+import Header from './Components/Header';
+import Content from './Components/Dashboard';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 import React from "react";
 import { Box } from "@mui/material";
+const theme = createTheme();
+
 
 const App = () => {
   return (
+    <ThemeProvider theme={theme}>
     <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
       <Header />
       <Box sx={{ display: "flex", flex: 1 }}>
         <Sidebar />
-        <Content/>
+        <Content />
         <Box sx={{ flex: 1, p: 3 }}>
         </Box>
       </Box>
     </Box>
+    </ThemeProvider>
   );
 };
 
